a20c96321ae06903beee2e32e91c4c272c32e4d5
Jorge Cisneros <fraxito@gmail.com>
2016-04-20 17:40:10 +0200

    Para mas pussiess

--- index.php
+++ index.php
@@ -8,23 +8,31 @@
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
     </head>
     <body>
         <?php
             $consulta_equipos = $mysqli -> query("SELECT * FROM equipos");
             
             $num_equipos = $consulta_equipos -> num_rows;
-            
+            echo '<table border="1">';
+            echo '<tr>';
+            echo '<th> id equipo</th>';
+            echo '<th> nombre equipo</th>';
+            echo '<th> ciudad equipo</th>';
+            echo '</tr>';
             for ($i = 0; $i < $num_equipos; $i++){
                $fila = $consulta_equipos -> fetch_array();
                $id_equipo = $fila['id_equipo'];
                $nombre_equipo = $fila['nombre'];
                $ciudad_equipo = $fila['ciudad'];
-               echo ' id equipo: '.$id_equipo;
-               echo ' nombre equipo: '.$nombre_equipo;
-               echo ' ciudad equipo: '.$ciudad_equipo;
-               echo '<br>';
+               echo '<tr>';
+               echo '<td>'.$id_equipo . '</td>';
+               echo '<td>'.$nombre_equipo. '</td>';
+               echo '<td>'.$ciudad_equipo. '</td>';
+               echo '</tr>';
             }
+            echo '</table>'
             
         ?>
     </body>
