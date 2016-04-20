d4e9ff55c65f3b9ca8166fdebbabe63a60f7f5b6
Jorge Cisneros <fraxito@gmail.com>
2016-04-20 18:20:49 +0200

    miercoles

--- index.php
+++ index.php
@@ -11,12 +11,17 @@
         <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css">
     </head>
     <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-xs-3"></div>
+                <div class="col-xs-6 ">
         <?php
             $consulta_equipos = $mysqli -> query("SELECT * FROM equipos");
             
             $num_equipos = $consulta_equipos -> num_rows;
-            echo '<table border="1">';
+            echo '<table class="table text-center table-striped table-bordered table-condensed">';
             echo '<tr>';
+            echo '<th> </th>';
             echo '<th> id equipo</th>';
             echo '<th> nombre equipo</th>';
             echo '<th> ciudad equipo</th>';
@@ -27,6 +32,7 @@
                $nombre_equipo = $fila['nombre'];
                $ciudad_equipo = $fila['ciudad'];
                echo '<tr>';
+               echo '<td><button class="btn btn-danger btn-xs"> <i class="glyphicon glyphicon-trash" ></i></button></td>';
                echo '<td>'.$id_equipo . '</td>';
                echo '<td>'.$nombre_equipo. '</td>';
                echo '<td>'.$ciudad_equipo. '</td>';
@@ -35,5 +41,14 @@
             echo '</table>'
             
         ?>
+                    </div>
+                <div class="col-xs-3"></div>
+                </div>
+        </div>
+        <script src="js/jquery-1.12.0.min.js"></script>
+        <script>
+            
+        </script>
+        
     </body>
 </html>
