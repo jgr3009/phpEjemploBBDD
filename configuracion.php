14c296173c8fb80c6fdb5764ddaebcfb75cc7402
Jorge Cisneros <fraxito@gmail.com>
2016-04-25 19:34:03 +0200

    Lunes con require

--- configuracion.php
+++ configuracion.php
@@ -1,6 +1,6 @@
 <?php
 $servidor = '127.0.0.1';
-$bd = 'liga';
+$bd = 'pruebasphp';
 $usuario_mysql = 'root';
 $clave_mysql = 'root';
 ?>
--- login.php
+++ login.php
@@ -5,12 +5,12 @@
     $nombreUsuario = $_POST['nombreUsuario'];
     $pass = $_POST['pass'];
             
-    $consulta = $mysqli -> query("SELECT * FROM equipos where id_equipo = '$nombreUsuario' and ciudad= '$pass';");
+    $consulta = $mysqli -> query("SELECT * FROM usuario where codigo = '$nombreUsuario' and pass= '$pass';");
 
     $num_filas = $consulta -> num_rows;
     
     if ($num_filas > 0){
-       echo '<h1> LOGIN CORRECTO </h1>';
+        require 'menuUsuario.php';
     }
     else {
         echo '<h1> anda y que te peinen  </h1>';
