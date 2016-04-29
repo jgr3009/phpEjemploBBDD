b2740de7edf6dd07738fcdb86e382ba461e50cd8
Jorge Cisneros <fraxito@gmail.com>
2016-04-29 17:10:03 +0200

    viernes

--- adminNuevoUsuario.php
+++ adminNuevoUsuario.php
@@ -9,27 +9,31 @@
         <br>
         <input type="email" class="form-control" id="email" placeholder="Email">
         <br>
-
-
-
-
+        <input type="text" class="form-control" id="DNI" placeholder="DNI">
+        <br>  
         <div class="dropdown">
-            <button id="botonTipo" class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Tipo
+            <button id="botonTipo" class="btn btn-default btn-block dropdown-toggle" type="button" data-toggle="dropdown">Tipo
                 <span class="caret"></span></button>
-            <ul class="dropdown-menu">
-                <li><a href="#">1 - Cliente</a></li>
-                <li><a href="#">2 - Administrador</a></li>
-                <li><a href="#">3 - Peluquero</a></li>
-                <li><a href="#">4 - Veterinario</a></li>
+            <ul class="dropdown-menu btn-block" >
+                <li><a href="#" onclick="cambiaTipo('1','Cliente')">1 - Cliente</a></li>
+                <li><a href="#" onclick="cambiaTipo('2','Administrador')">2 - Administrador</a></li>
+                <li><a href="#" onclick="cambiaTipo('3','Peluquero')">3 - Peluquero</a></li>
+                <li><a href="#" onclick="cambiaTipo('4','Veterinario')">4 - Veterinario</a></li>
             </ul>
         </div>
         <br>  
-        <input type="text" class="form-control" id="DNI" placeholder="DNI">
-        <br>   
+ 
     </div>
     <div class="col-xs-3"></div>
 </div>
-
+<script>
+    var tipo=0;
+    function cambiaTipo(_valor, _texto){
+        $('#botonTipo').html(_texto);
+        tipo = _valor;
+        console.log(tipo);
+    }
+</script>
 <?php
 
 
