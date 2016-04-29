7a43fca2dd418ec1683e87f262a447c1411f918f
Jorge Cisneros <fraxito@gmail.com>
2016-04-29 17:52:01 +0200

    viernes

---
+++ adminInsertaUsuario.php
@@ -0,0 +1,21 @@
+<?php
+    include('./funciones.php');
+    $mysqli = conectaBBDD();
+               
+    $nombreUsuario = $_POST['nombreUsuario'];
+    $pass = $_POST['pass'];
+    $email = $_POST['email'];
+    $DNI = $_POST['DNI'];
+    $tipo = $_POST['tipo'];
+            
+    $inserta_usuario = $mysqli -> 
+            query("INSERT INTO `veterinario`.`usuario` (`nombreUsuario`, `pass`, `email`, `tipo`, `DNI`) VALUES ('$nombreUsuario', '$pass', '$email', '$tipo', '$DNI'); ");
+
+    if ($mysqli->affected_rows == 1){
+        echo "Insertado correctamente el usuario con DNI " .$DNI;
+    }
+    else {
+        echo " ha ocurrido un error ". $mysqli->affected_rows ;
+    }
+    
+    ?>
--- adminNuevoUsuario.php
+++ adminNuevoUsuario.php
@@ -22,17 +22,40 @@
             </ul>
         </div>
         <br>  
- 
+        <button id="botonEnviar" class="btn btn-info btn-block " >Registrar</button>
     </div>
     <div class="col-xs-3"></div>
 </div>
+<br>
+<div class="row">
+    <div class="col-xs-3"></div>
+    <div class="col-xs-6" id="cargaResultado"></div>
+    <div class="col-xs-3"></div>
+</div>
 <script>
-    var tipo=0;
+    var _tipo=0;
     function cambiaTipo(_valor, _texto){
-        $('#botonTipo').html(_texto);
-        tipo = _valor;
-        console.log(tipo);
+        $('#botonTipo').html(_texto + ' <span class="caret"></span>');
+        _tipo = _valor;
+        console.log(_tipo);
     }
+    
+    $('#botonEnviar').click(function(){
+    var _nombreUsuario = $('#nombreUsuario').val();
+    var _pass = $('#pass').val();
+    var _email = $('#email').val();
+    var _DNI = $('#DNI').val();
+    
+
+        $('#cargaResultado').load('adminInsertaUsuario.php',{
+            nombreUsuario: _nombreUsuario,
+            pass : _pass,
+            email: _email,
+            DNI: _DNI,
+            tipo: _tipo
+        });
+    });
+            
 </script>
 <?php
 
