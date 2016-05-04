57019eac3434417e2bda8ebe188367e117fe350d
Jorge Cisneros <fraxito@gmail.com>
2016-05-04 15:31:12 +0200

    con jquery validate

--- adminNuevoUsuario.php
+++ adminNuevoUsuario.php
@@ -3,26 +3,28 @@
     <div class="col-xs-3"></div>
     <div class="col-xs-6">
         <br>
-        <input type="text" class="form-control" id="nombreUsuario" placeholder="Nombre Usuario">
-        <br>
-        <input type="password" class="form-control" id="pass" placeholder="Contraseña">
-        <br>
-        <input type="email" class="form-control" id="email" placeholder="Email">
-        <br>
-        <input type="text" class="form-control" id="DNI" placeholder="DNI">
-        <br>  
-        <div class="dropdown">
-            <button id="botonTipo" class="btn btn-default btn-block dropdown-toggle" type="button" data-toggle="dropdown">Tipo
-                <span class="caret"></span></button>
-            <ul class="dropdown-menu btn-block" >
-                <li><a href="#" onclick="cambiaTipo('1','Cliente')">1 - Cliente</a></li>
-                <li><a href="#" onclick="cambiaTipo('2','Administrador')">2 - Administrador</a></li>
-                <li><a href="#" onclick="cambiaTipo('3','Peluquero')">3 - Peluquero</a></li>
-                <li><a href="#" onclick="cambiaTipo('4','Veterinario')">4 - Veterinario</a></li>
-            </ul>
-        </div>
-        <br>  
-        <button id="botonEnviar" class="btn btn-info btn-block " >Registrar</button>
+        <form  id="nuevoUsuario" >
+            <input type="text" class="form-control" id="nombreUsuario" placeholder="Nombre Usuario">
+            <br>
+            <input type="password" class="form-control" id="pass" placeholder="Contraseña" required>
+            <br>
+            <input type="email" class="form-control" id="email" placeholder="Email">
+            <br>
+            <input type="text" class="form-control" id="DNI" placeholder="DNI">
+            <br>  
+            <div class="dropdown">
+                <button id="botonTipo" class="btn btn-default btn-block dropdown-toggle" type="button" data-toggle="dropdown">Tipo
+                    <span class="caret"></span></button>
+                <ul class="dropdown-menu btn-block">
+                    <li><a href="#" onclick="cambiaTipo('1','Cliente')">1 - Cliente</a></li>
+                    <li><a href="#" onclick="cambiaTipo('2','Administrador')">2 - Administrador</a></li>
+                    <li><a href="#" onclick="cambiaTipo('3','Peluquero')">3 - Peluquero</a></li>
+                    <li><a href="#" onclick="cambiaTipo('4','Veterinario')">4 - Veterinario</a></li>
+                </ul>
+            </div>
+            <br>  
+            <button id="botonEnviar" class="btn btn-info btn-block " >Registrar</button>
+        </form>
     </div>
     <div class="col-xs-3"></div>
 </div>
@@ -32,7 +34,13 @@
     <div class="col-xs-6" id="cargaResultado"></div>
     <div class="col-xs-3"></div>
 </div>
+<script src="js/jquery.validate.min.js"></script>
 <script>
+    
+    $('#nuevoUsuario').validate({
+        //submitHandler:
+    });
+    
     var _tipo=0;
     function cambiaTipo(_valor, _texto){
         $('#botonTipo').html(_texto + ' <span class="caret"></span>');
